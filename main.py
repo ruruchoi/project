f67ad67dbef7e8a38394bf5b31dca36844245855
ruruchoi <ruruzzang.yes@gmail.com>
2025-05-28 13:23:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
-import streamlit as st
-from random import randint
+import random
 
-attempt = 1
+#미션 숫자 생성
+def make_num(digits):
+    arrange = 0
+    mission_num = None
+    for _ in range(digits):
+        if arrange == 0:
+            mission_num = str(random.randint(1,9))
+            arrange = 1
+        else:
+            mission_num += str(random.randint(0,9))
+    return mission_num
 
-st.title('쉽게 즐기는 추론 게임')
+#숫자 비교
+def check(mission, guess, digits):
+    correct = 0
+    for i in range(digits):
+        if mission[i] == guess[i]:
+            print(f"{i + 1}번째 숫자 : 맞았어요!")
+            correct += 1
+        elif guess[i] in mission:
+            print(f"{i + 1}번째 숫자 : 다른 자리에 넣어주세요!")
+        else:
+            print(f"{i + 1}번 숫자 : 틀렸어요!")
+    return correct
 
-game_mode = st.selectbox("게임 유형을 선택하세요", ["숫자","알파벳"])
+game_mode = input("원하시는 게임을 선택해 주세요")
 
-if st.button('다음'):
-  if game_mode == '숫자':
-    num_digits = st.number_input("원하는 자릿수를 입력하세요", min_value=1, max_value=5, step=1)
-    min_num = 10**(num_digits-1)
-    max_num = int('9'*num_digits)
-    num = randint(min_num, max_num)
-    st.markdown(f"숫자는 {min_num}~{max_num}사이에 있습니다.")
+
+if game_mode == "숫자":
     while True:
-      num_input = st.number_input(f"{attempt}번째 시도", min_value = min_num, max_value = max_num, step=1)
-      attempt += 1
+    #자릿수 입력
+        attempt = 0
+        num_digits = int(input("자릿수를 입력하세요 : "))
+
+        mission_num = make_num(num_digits)
+
+        while True:
+            attempt += 1
+            my_guess = (input(f"{attempt}번째 시도 : "))
+            #자릿수 확인(첫 숫자 0 확인) 
+            if len(my_guess) != num_digits or not my_guess.isdigit():
+                print("입력이 잘못되었습니다. 다시 시도하세요.")
+                attempt -= 1
+                continue
+            if check(mission_num, my_guess, num_digits) == num_digits:
+                print(f"정답입니다! 축하합니다! 총 시도 횟수 : {attempt}번")
+                break
+
+        restart = input("게임을 다시 시작하시겠습니까? (y/n): ")
+        if restart != 'y':
+            print("게임을 종료합니다. 감사합니다!")
+            break
+else:
+    while True:
+        #영어 단어 결정
+        
+        
